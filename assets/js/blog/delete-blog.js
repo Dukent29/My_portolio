3062158469c8b6250604255c1c0251c79f79568b
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-05-04 19:31:00 +0200

    added animation && adding project/blogs by commands feature

--- assets/js/blog/delete-blog.js
+++ assets/js/blog/delete-blog.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
 
-const filePath = path.resolve(__dirname, "../../../blog.json");
+const filePath = path.resolve(__dirname, "../../../blogs.json");
 
 const rl = readline.createInterface({
   input: process.stdin,
