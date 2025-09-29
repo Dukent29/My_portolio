8f6a9736bf68ad27c73ce2dd95a3886b858a81a3
Kevin Bruce Mulinda <89967620+Dukent29@users.noreply.github.com>
2025-09-29 09:42:04 +0200

    Add single item detail page and enhance data
    Introduces single.html for detailed project/blog views with dynamic rendering. Updates blogs.json and projects.json to include date, tagline, and features fields. Refactors JS and CSS for improved badge spacing and action buttons, and updates links to use the new detail page.

--- assets/js/blog/blogs.js
+++ assets/js/blog/blogs.js
@@ -2,13 +2,13 @@ fetch('blogs.json')
   .then(response => response.json())
   .then(blogs => {
     const blogList = document.getElementById('blog-list');
-    blogList.innerHTML = blogs.map(blog => `
+    blogList.innerHTML = blogs.map((blog, index) => `
       <article class="blog card">
         <h5>${blog.title}</h5>
         <p>${blog.description}</p>
         <div class="actions">
           ${blog.tech.map(tech => `<span class="chip">${tech}</span>`).join('')}
-          <a class="btn btn-ghost" href="${blog.github_link}" target="_blank" rel="noopener">Code <i class="bi bi-arrow-up-right"></i></a>
+          <a class="btn btn-ghost" href="single.html?type=blogs&id=${index}" target="_blank" rel="noopener">Voir <i class="bi bi-arrow-up-right"></i></a>
         </div>
       </article>
     `).join('');
